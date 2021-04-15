d468bd94a7d42fa54f00de0c0efd84ab4736de32
brunosana <brunosa@dcomp.ufs.br>
2021-04-14 23:59:36 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import app from './app';
+
+app.listen(process.env.PORT || 3333, ()=> {
+    console.log(`Server started on port ${process.env.PORT}...`);
+});
