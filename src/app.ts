bf72f35648edf80bef61978398a6d9adb6b73424
brunosana <brunosa@dcomp.ufs.br>
2021-04-15 09:26:15 -0300

    feat: Create Error Handling method

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import AppError from './errors/AppError';
 import Routes from './routes';
 
 const app = express();
@@ -7,5 +9,18 @@ app.use(express.json());
 
 app.use(Routes);
 
+app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: 'Error',
+            message: error.message
+        });
+    }
+    console.error(error.message);
+    return response.status(500).json({
+        status: 'Error',
+        message: 'Internal server error'
+    })
+})
 
 export default app;
